48c729f16d356dda9ee26bcbb00c493b80b46c0b
Aeris <aeris.github@gmail.com>
2021-08-09 15:15:12 +0200

    Implemented the last two methods

--- WeatherIs.OpenWeatherMapApi/CurrentWeatherData.cs
+++ WeatherIs.OpenWeatherMapApi/CurrentWeatherData.cs
@@ -91,6 +91,41 @@ namespace WeatherIs.OpenWeatherMapApi
             return JsonConvert.DeserializeObject<CurrentWeatherDataResponse>(content);
         }
         
+        public async Task<CurrentWeatherDataResponse> GetWithinARectangleAsync(double longLeft, double latBottom, double longRight, double latTop, int zoom, UnitsType unitsType = UnitsType.Standard, CultureInfo culture = null)
+        {
+            culture ??= CultureInfo.CurrentCulture;
+            
+            var parameters = $"box/city?bbox={longLeft},{latBottom},{longRight},{latTop},{zoom}&appid={ApiKey}&units={Enum.GetName(unitsType)?.ToLower()}&lang={culture.TwoLetterISOLanguageName}";
+
+            var response = await Client.GetAsync(parameters);
+
+            if (!response.IsSuccessStatusCode)
+                throw new HttpRequestException($"Could not get weather data for box {longLeft},{latBottom},{longRight},{latTop},{zoom}", null, response.StatusCode);
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<CurrentWeatherDataResponse>(content);
+        }
+        
+        public async Task<CurrentWeatherDataResponse> GetWithinACircleAsync(double lat, double lon, int citiesNumber = 5, UnitsType unitsType = UnitsType.Standard, CultureInfo culture = null)
+        {
+            if (citiesNumber > 50)
+                throw new ArgumentOutOfRangeException(nameof(citiesNumber), citiesNumber, "Number of cities around the location cannot be above 50!");
+            
+            culture ??= CultureInfo.CurrentCulture;
+            
+            var parameters = $"find?lat={lat}&lon={lon}&cnt={citiesNumber}&appid={ApiKey}&units={Enum.GetName(unitsType)?.ToLower()}&lang={culture.TwoLetterISOLanguageName}";
+
+            var response = await Client.GetAsync(parameters);
+
+            if (!response.IsSuccessStatusCode)
+                throw new HttpRequestException($"Could not get weather data around coords {lat},{lon}", null, response.StatusCode);
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<CurrentWeatherDataResponse>(content);
+        }
+        
         public void Dispose()
         {
             Client.Dispose();
