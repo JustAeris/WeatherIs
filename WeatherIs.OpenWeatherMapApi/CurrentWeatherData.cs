e4925c3027f886a5517f7e48342baeb518dd761b
Aeris <aeris.github@gmail.com>
2021-08-09 10:44:20 +0200

    Implemented 4 methods to get the weather. More will be added in the future.

---
+++ WeatherIs.OpenWeatherMapApi/CurrentWeatherData.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Globalization;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using WeatherIs.OpenWeatherMapApi.Models;
+
+namespace WeatherIs.OpenWeatherMapApi
+{
+    public class CurrentWeatherData : IDisposable
+    {
+        private const string ApiUri = "https://api.openweathermap.org/data/2.5/";
+
+        public CurrentWeatherData(string apiKey)
+        {
+            ApiKey = apiKey;
+
+            Client = new HttpClient
+            {
+                BaseAddress = new Uri(ApiUri)
+            };
+
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        private string ApiKey { get; }
+        private HttpClient Client { get; }
+
+        public async Task<CurrentWeatherDataResponse> GetByCityNameAsync(string cityName, UnitsType unitsType = UnitsType.Standard, CultureInfo culture = null)
+        {
+            culture ??= CultureInfo.CurrentCulture;
+            
+            var parameters = $"weather?q={cityName}&appid={ApiKey}&units={Enum.GetName(unitsType)?.ToLower()}&lang={culture.TwoLetterISOLanguageName}";
+
+            var response = await Client.GetAsync(parameters);
+
+            if (!response.IsSuccessStatusCode)
+                throw new HttpRequestException($"Could not get weather data for {cityName}", null, response.StatusCode);
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<CurrentWeatherDataResponse>(content);
+        }
+        
+        public async Task<CurrentWeatherDataResponse> GetByCityIdAsync(int cityId, UnitsType unitsType = UnitsType.Standard, CultureInfo culture = null)
+        {
+            culture ??= CultureInfo.CurrentCulture;
+            
+            var parameters = $"weather?id={cityId}&appid={ApiKey}&units={Enum.GetName(unitsType)?.ToLower()}&lang={culture.TwoLetterISOLanguageName}";
+
+            var response = await Client.GetAsync(parameters);
+
+            if (!response.IsSuccessStatusCode)
+                throw new HttpRequestException($"Could not get weather data for ID {cityId}", null, response.StatusCode);
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<CurrentWeatherDataResponse>(content);
+        }
+        
+        public async Task<CurrentWeatherDataResponse> GetByCoordsAsync(double lat, double lon, UnitsType unitsType = UnitsType.Standard, CultureInfo culture = null)
+        {
+            culture ??= CultureInfo.CurrentCulture;
+            
+            var parameters = $"weather?lat={lat}&lon={lon}&appid={ApiKey}&units={Enum.GetName(unitsType)?.ToLower()}&lang={culture.TwoLetterISOLanguageName}";
+
+            var response = await Client.GetAsync(parameters);
+
+            if (!response.IsSuccessStatusCode)
+                throw new HttpRequestException($"Could not get weather data for coords {lat},{lon}", null, response.StatusCode);
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<CurrentWeatherDataResponse>(content);
+        }
+        
+        public async Task<CurrentWeatherDataResponse> GetByZipCodeAsync(int zipCode, RegionInfo country, UnitsType unitsType = UnitsType.Standard, CultureInfo culture = null)
+        {
+            culture ??= CultureInfo.CurrentCulture;
+            
+            var parameters = $"weather?zip={zipCode},{country.TwoLetterISORegionName.ToUpper()}&appid={ApiKey}&units={Enum.GetName(unitsType)?.ToLower()}&lang={culture.TwoLetterISOLanguageName}";
+
+            var response = await Client.GetAsync(parameters);
+
+            if (!response.IsSuccessStatusCode)
+                throw new HttpRequestException($"Could not get weather data for ZIP code {zipCode} in country {country.TwoLetterISORegionName}", null, response.StatusCode);
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<CurrentWeatherDataResponse>(content);
+        }
+        
+        public void Dispose()
+        {
+            Client.Dispose();
+        }
+    }
+}
