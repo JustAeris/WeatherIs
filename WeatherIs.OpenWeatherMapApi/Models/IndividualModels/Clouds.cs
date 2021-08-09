ff7fb87633eb5fe566b8a8baf8f70c3937e7f6c4
Aeris <aeris.github@gmail.com>
2021-08-09 10:43:37 +0200

    Added models to parse the json response from the API

---
+++ WeatherIs.OpenWeatherMapApi/Models/IndividualModels/Clouds.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace WeatherIs.OpenWeatherMapApi.Models.IndividualModels
+{
+    public class Clouds
+    {
+        [JsonProperty("all")]
+        public float Cloudiness { get; set; }
+    }
+}
---
+++ WeatherIs.OpenWeatherMapApi/Models/IndividualModels/Sys.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace WeatherIs.OpenWeatherMapApi.Models.IndividualModels
+{
+    public class Sys
+    {
+        [JsonProperty("country")]
+        public string CountryCode { get; set; }
+
+        [JsonProperty("sunrise")]
+        public int SunriseTime { get; set; }
+        
+        [JsonProperty("sunset")]
+        public int SunsetTime { get; set; }
+    }
+}
