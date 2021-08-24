088b7a17d2755d2d660957f6c3a3341fab95248f
Aeris <aeris.github@gmail.com>
2021-08-24 16:49:13 +0200

    Added configuration files

---
+++ WeatherIs.Web/Configuration/ConfigContext.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace WeatherIs.Web.Configuration
+{
+    public static class ConfigContext
+    {
+        static ConfigContext()
+        {
+            Config = JsonConvert.DeserializeObject<ConfigModel>(File.ReadAllText("config.json"));
+        }
+        
+        public static ConfigModel Config { get; }
+    }
+}
---
+++ WeatherIs.Web/Configuration/ConfigModel.cs
@@ -0,0 +1,7 @@
+﻿namespace WeatherIs.Web.Configuration
+{
+    public class ConfigModel
+    {
+        public string OpenWeatherMapApiKey { get; set; }
+    }
+}
