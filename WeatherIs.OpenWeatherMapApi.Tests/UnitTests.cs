f71e5bbb41bb02fe4cb3d8f723e9f515ebadf974
Aeris <aeris.github@gmail.com>
2021-08-27 15:34:13 +0200

    Modified the existing unit tests to implement the new API and renamed a field.

--- WeatherIs.OpenWeatherMapApi.Tests/UnitTests.cs
+++ WeatherIs.OpenWeatherMapApi.Tests/UnitTests.cs
@@ -12,101 +12,117 @@ namespace WeatherIs.OpenWeatherMapApi.Tests
 {
     public class Tests
     {
-        private CurrentWeatherData _client;
+        private CurrentWeatherData _currentWeatherClient;
+        private OneCallApi _oneCallApiClient;
         private string _apiKey;
-        
+
         [OneTimeSetUp]
         public void Setup()
         {
             var json = JObject.Parse(File.ReadAllText("appsettings.json"));
 
             _apiKey = (string) json["OpenWeatherMapApiKey"];
-            _client = new CurrentWeatherData(_apiKey);
+            _currentWeatherClient = new CurrentWeatherData(_apiKey);
+            _oneCallApiClient = new OneCallApi(_apiKey);
         }
 
         [OneTimeTearDown]
         public void TearDown()
         {
-            _client.Dispose();
+            _currentWeatherClient.Dispose();
         }
 
         [Test]
         public async Task ByCityName()
         {
-            var amsterdam = await _client.GetByCityNameAsync("amsterdam");
-            var paris = await _client.GetByCityNameAsync("paris", UnitsType.Metric, new CultureInfo("fr"));
-            var london = await _client.GetByCityNameAsync("london", UnitsType.Imperial);
+            var amsterdam = await _currentWeatherClient.GetByCityNameAsync("amsterdam");
+            var paris = await _currentWeatherClient.GetByCityNameAsync("paris", UnitsType.Metric, new CultureInfo("fr"));
+            var london = await _currentWeatherClient.GetByCityNameAsync("london", UnitsType.Imperial);
             Assert.IsNotNull(amsterdam);
             Assert.IsNotNull(paris);
             Assert.IsNotNull(london);
-            
+
             Assert.ThrowsAsync<HttpRequestException>(async () =>
-                await _client.GetByCityNameAsync("this city does not exists"));
+                await _currentWeatherClient.GetByCityNameAsync("this city does not exists"));
         }
-        
+
         [Test]
         public async Task ByCityId()
         {
-            var city1 = await _client.GetByCityIdAsync(685445);
-            var city2 = await _client.GetByCityIdAsync(6535826, UnitsType.Metric, new CultureInfo("it"));
-            var city3 = await _client.GetByCityIdAsync(1798725, UnitsType.Imperial);
+            var city1 = await _currentWeatherClient.GetByCityIdAsync(685445);
+            var city2 = await _currentWeatherClient.GetByCityIdAsync(6535826, UnitsType.Metric, new CultureInfo("it"));
+            var city3 = await _currentWeatherClient.GetByCityIdAsync(1798725, UnitsType.Imperial);
             Assert.IsNotNull(city1);
             Assert.IsNotNull(city2);
             Assert.IsNotNull(city3);
-            
+
             Assert.ThrowsAsync<HttpRequestException>(async () =>
-                await _client.GetByCityIdAsync(750149850));
+                await _currentWeatherClient.GetByCityIdAsync(750149850));
         }
-        
+
         [Test]
         public async Task ByCoords()
         {
-            var city = await _client.GetByCoordsAsync(48.864716, 2.349014);
+            var city = await _currentWeatherClient.GetByCoordsAsync(48.864716, 2.349014);
             Assert.IsNotNull(city);
-            
+
             Assert.ThrowsAsync<HttpRequestException>(async () =>
-                await _client.GetByCoordsAsync(75014560, double.Epsilon));
+                await _currentWeatherClient.GetByCoordsAsync(75014560, double.Epsilon));
         }
-        
+
         [Test]
         public async Task ByZipCode()
         {
-            var city = await _client.GetByZipCodeAsync(75000, new RegionInfo("fr"));
+            var city = await _currentWeatherClient.GetByZipCodeAsync(75000, new RegionInfo("fr"));
             Assert.IsNotNull(city);
-            
+
             Assert.ThrowsAsync<HttpRequestException>(async () =>
-                await _client.GetByZipCodeAsync(75014560, new RegionInfo("fr")));
+                await _currentWeatherClient.GetByZipCodeAsync(75014560, new RegionInfo("fr")));
         }
 
         [Test]
         public async Task WithinARectangle()
         {
-            var city = await _client.GetWithinARectangleAsync(12, 32, 15, 37, 10);
+            var city = await _currentWeatherClient.GetWithinARectangleAsync(12, 32, 15, 37, 10);
             Assert.IsNotNull(city);
 
             Assert.ThrowsAsync<HttpRequestException>(async () =>
-                await _client.GetWithinARectangleAsync(987, 465, 65, 645, 4));
+                await _currentWeatherClient.GetWithinARectangleAsync(987, 465, 65, 645, 4));
         }
-        
+
         [Test]
         public async Task WithinACircle()
         {
-            var city = await _client.GetWithinACircleAsync(12, 32, 15);
+            var city = await _currentWeatherClient.GetWithinACircleAsync(12, 32, 15);
             Assert.IsNotNull(city);
 
             Assert.ThrowsAsync<ArgumentOutOfRangeException>(async () =>
-                await _client.GetWithinACircleAsync(55, 37, 65));
-            
+                await _currentWeatherClient.GetWithinACircleAsync(55, 37, 65));
+
             Assert.ThrowsAsync<HttpRequestException>(async () =>
-                await _client.GetWithinACircleAsync(874, 211));
+                await _currentWeatherClient.GetWithinACircleAsync(874, 211));
         }
 
         [Test]
         public async Task TestCityList()
         {
             await CityListRetriever.RetrieveCityList();
-            
+
             Assert.IsTrue(CityListRetriever.CityList.Any());
+        }
+
+        [Test]
+        public async Task OneCallApi()
+        {
+            if (CityListRetriever.CityList == null)
+                await CityListRetriever.RetrieveCityList();
+
+            var city = (CityListRetriever.CityList ?? throw new InvalidOperationException()).First(c => c.Name == "Paris" && c.Country == "FR");
+
+            var forecast =
+                await _oneCallApiClient.GetByCoordsAsync(city.Coords.Latitude, city.Coords.Longitude, UnitsType.Metric);
+
+            Assert.IsNotNull(forecast);
         }
     }
 }
