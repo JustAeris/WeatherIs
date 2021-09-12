b6c897d4b9d0dc77ea21bd972cd59bc84f20309f
Aeris <aeris.github@gmail.com>
2021-09-12 18:31:52 +0200

    Added properties for certain units

--- WeatherIs.Web/Models/ForecastViewModel.cs
+++ WeatherIs.Web/Models/ForecastViewModel.cs
@@ -9,6 +9,17 @@ namespace WeatherIs.Web.Models
         public string ErrorMessage { get; set; }
 
         public bool MetricUnits { get; set; }
+        public string TemperatureUnit => MetricUnits switch
+        {
+            true => "°C",
+            false => "°F"
+        };
+
+        public string WindSpeedUnit => MetricUnits switch
+        {
+            true => "km/h",
+            false => "mph"
+        };
 
         public bool IsUsingAutoGeolocation { get; set; }
     }
