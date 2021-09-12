91091d1f7959be667bbe107beba15824bd8ff9aa
Aeris <aeris.github@gmail.com>
2021-09-12 18:29:01 +0200

    Added memory cache

---
+++ WeatherIs.Web/Models/CacheKeys.cs
@@ -0,0 +1,8 @@
+﻿namespace WeatherIs.Web.Models
+{
+    public static class CacheKeys
+    {
+        public static string CurrentWeatherData => "_CurrentWeatherData_";
+        public static string Forecast => "_Forecast_";
+    }
+}
--- WeatherIs.Web/Startup.cs
+++ WeatherIs.Web/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -20,6 +21,7 @@ namespace WeatherIs.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddMemoryCache();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
